feb9044b1977344181c5e9bf7121a2d59f5c5a0e
Mike Schutte <tmikeschutte@gmail.com>
2020-05-24 12:31:28 -0400

    Update cache hash

--- public/worker.js
+++ public/worker.js
@@ -1,20 +1,20 @@
-var CACHE_NAME = "workout-timer:1.0.6";
+var CACHE_NAME = "workout-timer:1.0.7";
 var urlsToCache = ["/"];
 
 // Install a service worker
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
 // Cache and return requests
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       // Cache hit - return response
       if (response) {
         return response;
@@ -25,12 +25,12 @@ self.addEventListener("fetch", event => {
 });
 
 // Update a service worker
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   var cacheWhitelist = ["workout-timer"];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
@@ -40,7 +40,7 @@ self.addEventListener("activate", event => {
   );
 });
 
-self.addEventListener("message", event => {
+self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
